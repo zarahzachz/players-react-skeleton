c3c76bf550682568144e66e1d8fdb1022ae5f100
Sarah <sarah.sachs@gmail.com>
2019-02-01 17:21:03 -0500

    Initial project setup (routes, Scss, fix npm error).

---
+++ src/pages/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Welcome from './Welcome';
+import Register from './Register';
+import Login from './Login';
+import Roster from './Roster';
+import NewPlayer from './NewPlayer';
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Welcome} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/roster" component={Roster} />
+      <Route exact path="/player/new" component={NewPlayer} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;

8af43d673f4437c81ec5db69887905ac406c8429
Sarah <sarah.sachs@gmail.com>
2019-02-04 16:20:27 -0500

    Turn welcome into arrow function.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import PageHeader from '../components/PageHeader';
 
-class Welcome extends Component {
-  constructor(props) {
-    super(props);
-    this.goTo = this.goTo.bind(this);
-  }
-
-  goTo(event, url) {
-    event.preventDefault();
-    this.props.history.push(`/${url}`);
-  }
-
-  render() {
-    return (
-      <div>
-        <PageHeader title="Welcome" />
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
-    );
-  }
-}
+const Welcome = () => (
+  <div>
+    <PageHeader title="Welcome" />
+    <Link to="/login">Login</Link>
+    <Link to="/register">Register</Link>
+  </div>
+);
 
 export default Welcome;

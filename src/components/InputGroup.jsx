d289c229a68139336452076855878cff3ad54174
Sarah <sarah.sachs@gmail.com>
2019-02-05 16:58:17 -0500

    Style inputs.

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import styles from '../styles';
+
+const Field = styled.div`
+  font-family: ${styles.font.base};
+
+  label {
+    color: rgba(255, 255, 255, 0.8);
+    display: flex;
+    font-size: 0.75rem;
+    flex-direction: column;
+  }
+
+  input {
+    border: none;
+    flex: 0 0 auto;
+    font-size: 1rem;
+    height: 2rem;
+    margin-top: 0.5rem;
+  }
+`;
 
 const InputGroup = props => (
-  <div>
+  <Field>
     <label htmlFor={props.id}>
       {props.label}
       <input
@@ -13,7 +34,7 @@ const InputGroup = props => (
         onChange={props.change}
       />
     </label>
-  </div>
+  </Field>
 );
 
 InputGroup.propTypes = {

b5d541e2a9af436d6d389f24d5fc9c2ccaad78e1
Sarah <sarah.sachs@gmail.com>
2019-02-06 09:57:11 -0500

    Style 404 and refactor to use components.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import styles from '../styles';
 
 import image from '../assets/images/404.gif';
+import PageHeader from '../components/PageHeader';
+
+const Image = styled.img`
+  height: auto;
+  width: 100%;
+`;
+
+const Info = styled.p`
+  font-size: 1.25rem;
+  margin: 0;
+  text-align: center;
+  text-transform: uppercase;
+
+  @media screen and (min-width: ${styles.screen.mobile}) {
+    font-size: 1.5rem;
+  }
+`;
 
 const NotFound = () => (
-  <div>
-    <h1>404 :(</h1>
-    <p>Page not found.</p>
-    <img src={image} alt="Page not found - try going somewhere else." />
-  </div>
+  <React.Fragment>
+    <PageHeader title="404 :(" />
+    <Info>Page not found.</Info>
+    <Image src={image} alt="Page not found - try going somewhere else." />
+  </React.Fragment>
 );
 
 export default NotFound;
